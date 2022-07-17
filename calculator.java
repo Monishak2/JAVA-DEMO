cfb4f92edb7de67cea4b2a51896be26354082e2d
Monishak2 <109268559+Monishak2@users.noreply.github.com>
2022-07-17 19:55:34 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,33 @@
+package code;
+
+	import java.util.Scanner;
+	public class calculator {
+	   public static void main(String[] args) {
+	      double no1;
+	      double no2;
+	      double result;
+	      char op;
+	      Scanner sc = new Scanner(System.in);
+	      System.out.print("Enter inputs: ");
+	      no1 =sc.nextDouble();
+	      no2 = sc.nextDouble();
+	      System.out.print("\nEnter an operator (+, -, *, /): ");
+	      op = sc.next().charAt(0);
+	      switch(op) {
+	         case '+': result = no1 + no2;
+	            break;
+	         case '-': result = no1 - no2;
+	            break;
+	         case '*': result = no1 * no2;
+	            break;
+	         case '/': result= no1 / no2;
+	            break;
+	      default: System.out.printf("INVALID OPERATOR");
+	         return;
+	      }
+	      System.out.print("\nThe answer is:\n");
+	      System.out.printf(no1 + " " + op + " " + no2 + " = " + result);
+	   }
+	}
+
+
